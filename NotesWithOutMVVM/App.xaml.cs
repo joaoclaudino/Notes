fa1072f32609b192c6cedacb1d4d502137a89597
joaoclaudino <joao.claudino@uol.com.br>
2023-05-07 13:39:44 +0100

    init

---
+++ NotesWithOutMVVM/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        MainPage = new AppShell();
+	}
+}
